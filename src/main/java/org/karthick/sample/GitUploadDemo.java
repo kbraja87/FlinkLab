ce4206042aa9a169dc834e336131dca34306df2c
KB <karthick.learns@gmail.com>
2020-05-15 22:51:21 +0530

    Second Commit

--- src/main/java/org/karthick/sample/GitUploadDemo.java
+++ src/main/java/org/karthick/sample/GitUploadDemo.java
@@ -3,5 +3,6 @@ package org.karthick.sample;
 public class GitUploadDemo {
     public static void main(String[] args) {
         System.out.println("Git Upload Demo");
+        System.out.println("Git Upload second");
     }
 }
